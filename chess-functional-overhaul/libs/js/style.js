c32c050b264dedc3adcf5e44dff8cd48d43903c8
jake-lonceras <loncerasjakerei@gmail.com>
2022-02-02 21:06:07 +0800

    Added function for part styling

--- chess-functional-overhaul/libs/js/style.js
+++ chess-functional-overhaul/libs/js/style.js
@@ -1,9 +1,18 @@
 
-export default function style(classNameParam, style, pseudo = ``) {
+export function style(classNameParam, style, pseudo = ``) {
     const className = classNameParam;
     const styleElement = document.createElement(`style`);
     styleElement.textContent = `.${className} { ${style} } ${pseudo}`;
     document.head.appendChild(styleElement);
     return className;
+}
+
+
+export function part(customElementName, partNameParam, style, pseudo = ``) {
+    const partName = partNameParam;
+    const styleElement = document.createElement(`style`);
+    styleElement.textContent = `${customElementName}::part(${partNameParam}) { ${style} } ${pseudo}`;
+    document.head.appendChild(styleElement);
+    return partName;
 }
 
